1467bdf5b4f11ad9f48714e46433fdb9e3f365d3
shkch02 <cba7215@gmail.com>
2026-01-15 01:15:51 +0900

    기본 필기로

--- lib/models/session_form.dart
+++ lib/models/session_form.dart
@@ -11,7 +11,7 @@ class ExerciseForm {
   List<String> photos = [];
 
   /// 세트 입력 모드 (디지털 입력 또는 필기 입력)
-  SetInputMode setInputMode = SetInputMode.digital;
+  SetInputMode setInputMode = SetInputMode.handwriting;
 
   /// 필기 모드 사용 시 저장된 이미지 경로
   String? handwritingImagePath;
